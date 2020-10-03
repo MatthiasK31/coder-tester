39daacc62031f0a5a682cd6c50f465e8019f6e18
matthiask31 <matthiastk805@gmail.com>
2020-10-02 20:37:29 -0400

    no

---
+++ src/pairing.java
@@ -0,0 +1,181 @@
+/*
+Matthias Kim
+Tester/Coder Lab
+9/18/2020
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
+
+public class pairing {
+    public static void main(String[] args) {
+
+        try {
+            //setup with variables
+            File classNames = new File("src/classList");
+            Scanner s = new Scanner(System.in);
+            Scanner reader = new Scanner(classNames);
+            int totalStudents = 0, b1 = 0, b2 = 1;
+
+            //ask for user input to determine the block used
+            System.out.println("\nThis program will randomly match coders and testers from each/both blocks.");
+            System.out.println("Please type the respective number for which block(s)'s pairs you'd like to see.\n1) Block 1 \n2) Block 2 \n3) Blocks 1 & 2");
+            int blockChoice = s.nextInt();
+
+            //run loop until it has gone through the entire file
+            //add to a variable for block 1/block2
+            do {
+                String tempReader = reader.nextLine();
+                if (tempReader.charAt(0) == '1') {
+                    b1++;
+                }
+                if (tempReader.charAt(0) == '2') {
+                    b2++;
+                }
+            } while (reader.hasNextLine());
+            totalStudents = b1 + b2;
+
+            //print the info for the user to see
+            switch (blockChoice) {
+                case 1: {
+                    System.out.println("Block 1 has " + b1 + " total students.");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Block 2 has " + b2 + " total students.");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Block 1 has " + b1 + " students.");
+                    System.out.println("Block 2 has " + b2 + " students.");
+                    System.out.println("There are " + totalStudents + " total students.");
+                    break;
+                }
+
+            }
+
+            //reset the scanner
+            reader = new Scanner(classNames);
+
+            //put all the names from the file into an array
+            String [] studentNames = new String[totalStudents + 1];
+            for (int i = 0; i < totalStudents; i++) {
+                studentNames[i] = reader.nextLine();
+            }
+
+            //determine which area of the file is going to be read
+            String [] selectionRange = { };
+            int startRead = 0, endRead = 0;
+            int tempCounter;
+            char tempChar = ' ';
+            if (blockChoice == 1)
+            {
+                endRead = b1;
+                tempChar = '1';
+
+                selectionRange = new String [endRead - startRead];
+                tempCounter = 0;
+                for (int i = startRead; i < endRead; i++) {
+                    if (studentNames[i].charAt(0) == tempChar){
+                        selectionRange[tempCounter] = studentNames[i].substring(2);
+                        tempCounter++;
+                    }
+                    //exit the loop once the block 1/2 names are done
+                    else{
+                        break;
+                    }
+                }
+            }
+            else if (blockChoice == 2) {
+                startRead = b1;
+                endRead = b1 + b2;
+                tempChar = '2';
+                tempCounter = 0;
+                for (int i = startRead; i < endRead; i++) {
+                    if (studentNames[i].charAt(0) == tempChar){
+                        selectionRange[tempCounter] = studentNames[i].substring(2);
+                        tempCounter++;
+                    }
+                    //exit the loop once the block 1/2 names are done
+                    else{
+                        break;
+                    }
+                }
+            }
+            else if (blockChoice == 3) {
+                selectionRange = new String[b1 + b2];
+                tempCounter = 0;
+                do {
+                    selectionRange[tempCounter] = studentNames[tempCounter].substring(2);
+                    tempCounter++;
+                } while (tempCounter < b1 + b2);
+            }
+
+            String [] coders = new String[selectionRange.length];
+            String [] testers = new String[selectionRange.length];
+            //a boolean to tell whether the coder/tester has been taken
+            boolean [] isTaken = new boolean[selectionRange.length];
+
+            //take all the names out of the desired selection (block)
+            int increment = 0;
+            do{
+                coders[increment] = selectionRange[increment];
+                increment++;
+            } while (increment < selectionRange.length);
+
+
+            for (int i = 0; i < selectionRange.length; i++) {
+                int testerPlace =  (int) Math.floor(Math.random() * selectionRange.length);
+                if (coders[testerPlace].equals(testers[testerPlace])){
+                    //changes the tester array placer at least once, but keeps going until the coder and tester are not equal
+                    do {
+                        testerPlace =  (int) Math.floor(Math.random() * selectionRange.length);
+                    } while(coders[testerPlace].equals(testers[testerPlace]));
+                }
+                //if the names are already taken reroll the number
+                else if (isTaken[testerPlace]){
+                    do {
+                        testerPlace =  (int) Math.floor(Math.random() * selectionRange.length);
+                    } while(isTaken[testerPlace]);
+                }
+
+                //check whether the coder & tester do not match, then set the bool to true and rerun the loop
+                if (!coders[testerPlace].equals(testers[testerPlace])){
+                    isTaken[testerPlace] = true;
+                    testers[i] = selectionRange[testerPlace];
+                }
+            }
+
+            for (int i = 0; i < selectionRange.length; i++)
+            {
+                for (int j = i + 1; j < selectionRange.length; j++)
+                {
+                    if (coders[i].compareTo(coders[j]) > 0)
+                    {
+                        String temp = coders[i];
+                        String temp2 = testers[i];
+                        coders[i] = coders[j];
+                        testers[i] = testers[j];
+                        coders[j] = temp;
+                        testers[j] = temp2;
+                    }
+                }
+            }
+
+            System.out.printf("%-15s %-30s", "Coder", "Tester");
+            System.out.println("\n-----------------------------------------------------------");
+            for (int i = 0; i < coders.length; i++) {
+                System.out.format("%-30s %-30s", coders[i], testers[i]);
+                System.out.println();
+            }
+
+
+
+
+
+        } catch (FileNotFoundException f) {
+            System.out.println(f.toString());
+        }
+    }
+}
---
+++ src/vlamk.java
@@ -0,0 +1,230 @@
+// imports
+import java.util.Scanner;
+import java.io.*;
+
+public class vlamk {
+    public static void main(String[] args) {
+        // Setup
+        long startTime = 0; // for timing purposes
+        try {
+            String classlist = "src/classList";
+            Scanner in = new Scanner(System.in);
+            BufferedReader read = new BufferedReader(new FileReader(classlist)); //pass files in as a commandline argument
+            int students = 0, block1 = 0, block2 = 0;
+
+            // block selection menu
+            System.out.print("What block would you like to select testers for? \n1) Block 1\n2) Block 2\n3) Both blocks\nType the number for your desired choice: ");
+            int blockChoice = in.nextInt();
+            // Ask the user how they want to sort the output (coder, tester, both)
+            System.out.println("How do you want to sort the output? By\n1)coder name\n2)tester name or\n3) would you like to see both?");
+            int sortChoice = in.nextInt();
+
+            startTime = System.nanoTime(); // this is for timing purposes.
+
+            // how many people are in the file?
+            String line = read.readLine();
+            do {
+                // System.out.println(students + ")   " + line);
+                switch (line.charAt(0)) {
+                    case '1':
+                        block1++;
+                        break;
+                    case '2':
+                        block2++;
+                        break;
+                }
+                line = read.readLine();
+                students++;
+            } while (line != null);
+            System.out.println("Block 1: " + block1);
+            System.out.println("Block 2: " + block2);
+            System.out.println("Student total: " + students);
+            read.close();
+
+            // check if the input is valid
+            // side note: this only checks if the block numbers are valid. it does NOT check for the
+            // comma afterwards, nor for the name to be in the correct format.
+            if (students != block1 + block2) {
+                System.out.println("The file format is invalid.");
+                System.exit(1);
+            } else {
+                System.out.println("The file format is valid, continuing...\n\n");
+            }
+
+            // initialize an array to make data processing easier
+
+            String[] people = new String[students+1];
+
+            // now we reset the Scanner
+            read = new BufferedReader(new FileReader(classlist));
+
+            // Turn the file into an array so we don't have to keep reading it
+            line = read.readLine();
+            int counter = 0;
+            do {
+                people[counter] = line;
+                line = read.readLine();
+                counter++;
+            } while (line != null);
+            read.close();
+
+
+            // Now this is where the actual data processing begins.
+            // This snippet chops up all the data to prepare it to be processed.
+            int startat = 0, runto = 0;
+            char block = 'a';
+            String[] selectionSet = {};
+            if (blockChoice == 1 | blockChoice == 2) {
+                switch (blockChoice) {
+                    case 1:
+                        runto = block1;
+                        block = '1';
+                    case 2:
+                        startat = block1;
+                        runto = block2 + block1;
+                        block = '2';
+                }
+                selectionSet = new String[runto-startat];
+                int k = 0;
+                for (int i = 0; i < runto; i++) {
+                    if (people[i].charAt(0) == block) {
+                        selectionSet[k] = people[i].substring(2);
+                        k++;
+                    }
+                }
+            } else if (blockChoice == 3) {
+                selectionSet = new String[block1 + block2];
+                for (int i = 0; i < block1+block2; i++) {
+                    selectionSet[i] = people[i].substring(2);
+                }
+            }
+
+            // Now it's time to assign coders and testers.
+            String[] coders = new String[selectionSet.length];
+            String[] testers = new String[selectionSet.length];
+            // "why you... why you boolean me" - not s1mple
+            boolean[] takenTester = new boolean[selectionSet.length];
+
+            for (int i = 0; i < selectionSet.length; i++) coders[i] = selectionSet[i];
+
+
+            // Now we generate the pairs.
+            // Did someone say "DNA building?"
+            // Because if you did, please leave now.
+            for (int i = 0; i < selectionSet.length; i++) {
+                // This whole section is for avoiding duplicates.
+                int tester = (int) Math.floor(Math.random()*selectionSet.length);
+                if (coders[tester].equals(testers[tester])) {
+                    while(coders[tester].equals(testers[tester])) {
+                        tester = (int) Math.floor(Math.random()*selectionSet.length);
+                    }
+                }
+                if (takenTester[tester]) {
+                    while (takenTester[tester]) {
+                        tester = (int) Math.floor(Math.random()*selectionSet.length);
+                    }
+                }
+                // Once a new pair with zero duplicates has been chosen,
+                // those slots are marked as "taken" by the program
+                // so they don't get taken again
+                takenTester[tester] = true;
+                testers[i] = selectionSet[tester];
+            }
+
+
+
+            // Sorting and output
+            if (sortChoice == 1) { // sort by coder
+                for (int i = 0; i < selectionSet.length; i++) {
+                    for (int j = i + 1; j < selectionSet.length; j++) {
+                        if (coders[i].compareTo(coders[j]) > 0) {
+                            String temp = coders[i];
+                            String temp2 = testers[i];
+                            coders[i] = coders[j];
+                            testers[i] = testers[j];
+                            coders[j] = temp;
+                            testers[j] = temp2;
+                        }
+                    }
+                }
+                System.out.format("%-30s %-30s", "Coder", "Tester");
+                System.out.println("\n-----------------------------------------------------------");
+                for (int i = 0; i < coders.length; i++) {
+                    System.out.format("%-30s %-30s", coders[i], testers[i]);
+                    System.out.println();
+                }
+            } else if (sortChoice == 2) { // sort by tester
+                for (int i = 0; i < selectionSet.length; i++) {
+                    for (int j = i + 1; j < selectionSet.length; j++) {
+                        if (testers[i].compareTo(testers[j]) > 0) {
+                            String temp = coders[i];
+                            String temp2 = testers[i];
+                            coders[i] = coders[j];
+                            testers[i] = testers[j];
+                            coders[j] = temp;
+                            testers[j] = temp2;
+                        }
+                    }
+                }
+                System.out.format("%-30s %-30s", "Tester", "Coder");
+                System.out.println("\n-----------------------------------------------------------");
+                for (int i = 0; i < coders.length; i++) {
+                    System.out.format("%-30s %-30s", testers[i], coders[i]);
+                    System.out.println();
+                }
+            } else if (sortChoice == 3) { // sort by coder, print, sort by tester, and print
+                for (int i = 0; i < selectionSet.length; i++) {
+                    for (int j = i + 1; j < selectionSet.length; j++) {
+                        if (coders[i].compareTo(coders[j]) > 0) {
+                            String temp = coders[i];
+                            String temp2 = testers[i];
+                            coders[i] = coders[j];
+                            testers[i] = testers[j];
+                            coders[j] = temp;
+                            testers[j] = temp2;
+                        }
+                    }
+                }
+                System.out.format("%-30s %-30s", "Coder", "Tester");
+                System.out.println("\n-----------------------------------------------------------");
+                for (int i = 0; i < coders.length; i++) {
+                    System.out.format("%-30s %-30s", coders[i], testers[i]);
+                    System.out.println();
+                }
+                System.out.println("\n\n");
+                for (int i = 0; i < selectionSet.length; i++) {
+                    for (int j = i + 1; j < selectionSet.length; j++) {
+                        if (testers[i].compareTo(testers[j]) > 0) {
+                            String temp = coders[i];
+                            String temp2 = testers[i];
+                            coders[i] = coders[j];
+                            testers[i] = testers[j];
+                            coders[j] = temp;
+                            testers[j] = temp2;
+                        }
+                    }
+                }
+                System.out.format("%-30s %-30s", "Tester", "Coder");
+                System.out.println("\n-----------------------------------------------------------");
+                for (int i = 0; i < coders.length; i++) {
+                    System.out.format("%-30s %-30s", testers[i], coders[i]);
+                    System.out.println();
+                }
+            } else {
+                System.out.println("Invalid result provided. Exiting...");
+                System.exit(1);
+            }
+
+        } catch (FileNotFoundException fne) {
+            // Certain Java functions tend to get a little angery™
+            // and start throwing stuff called "exceptions" around.
+            // Luckily, this catch statement used to be a professional baseball catcher.
+            System.out.println(fne.toString());
+        } catch (IOException ioe) {
+            System.out.println(ioe.toString());
+        }
+        long endTime = System.nanoTime();
+        long timeToRun = (endTime-startTime) / 1000;
+        System.out.println("Execution time (µs): " + timeToRun);
+    }
+}
